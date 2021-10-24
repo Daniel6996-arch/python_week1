8ff2aea387f22680556c783265f7e82b92cf46f5
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-10-24 15:22:44 +0300

    add test to test save_multiple contacts

--- test.py
+++ test.py
@@ -23,8 +23,14 @@ class TestUser(unittest.TestCase):
         the user list
         '''
         self.new_user.save_user()
-        self.assertEqual(len(User.user_list),1
-        
+        self.assertEqual(len(User.user_list),1)
+
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            User.user_list = []    
+
     def test_save_multiple_user(self):
             '''
             test_save_multiple_user to check if we can save multiple user
@@ -33,7 +39,7 @@ class TestUser(unittest.TestCase):
             self.new_user.save_user()
             test_user = User("Test_User","test@user.com") # new user
             test_user.save_user()
-            self.assertEqual(len(User.user_list),2)     
+            self.assertEqual(len(User.user_list),2)
 
 
 if __name__ == '__main__':
