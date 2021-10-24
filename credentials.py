bd08e6665dd1543341fa96f930e7e943ba0e228c
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-10-24 16:02:24 +0300

    add initial credentials class

---
+++ credentials.py
@@ -0,0 +1,38 @@
+class Credentials:
+    """
+    Class that generates new instances of credentials.
+    """
+    credentials_list = []
+
+
+    def __init__(self,account_name,user_name,email,password):
+        """
+        create arguments for self
+        """
+        self.account_name = user_name
+        self.user_name = user_name
+        self.email = email
+        self.password = password
+       
+
+    def save_user_credentials(self):
+        """ 
+        save_user_credentials method saves user credentials objects into credentials_list
+        """
+        Credentials.credentials_list.append(self)
+
+    def delete_user_credentials(self):
+
+        '''
+        delete_user_credentials method deletes a saved credentials from the credentials_list
+        '''
+
+        User.user_list.remove(self) 
+        
+    @classmethod
+    def display_users(cls):
+        '''
+        method that returns the user list
+        '''
+        return cls.user_list        
+      
