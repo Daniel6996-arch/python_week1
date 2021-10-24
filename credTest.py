e30a416d9dbd9fbb5ea56c2222bf309bbfaf6999
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-10-24 17:43:10 +0300

    add inal test to test module

--- credTest.py
+++ credTest.py
@@ -7,7 +7,7 @@ class TestCreds(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_account = Credentials("Twitter","John-arch","John.arch@email.com","1234567890k") # create user objec
+        self.new_account = Credentials("Twitter","John-arch","john.arch@email.com","1234567890k") # create user objec
     
     def test_init(self):
         '''
@@ -16,10 +16,50 @@ class TestCreds(unittest.TestCase):
 
         self.assertEqual(self.new_account.account_name,"Twitter")
         self.assertEqual(self.new_account.user_name,"John-arch")
-        self.assertEqual(self.new_account.email,"John.arch@email.com")
+        self.assertEqual(self.new_account.email,"john.arch@email.com")
         self.assertEqual(self.new_account.password,"1234567890k")
 
-                 
+    def test_save_user_account(self):
+        '''
+        test_save_user_account test case to test if the account object is saved into
+        the accounts list
+        '''
+        self.new_account.save_user_account()
+        self.assertEqual(len(Credentials.accounts_list),1)
+
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            Credentials.accounts_list = []    
+
+    def test_save_multiple_user_accounts(self):
+            '''
+            test_save_multiple_user_accounts to check if we can save multiple user accounts
+            objects to our accounts_list
+            '''
+            self.new_account.save_user_account()
+            test_account = Credentials("Twit","Ian-arc","john.arch@email.com","1234567890k") # new user
+            test_account.save_user_account()
+            self.assertEqual(len(Credentials.accounts_list),2)
+
+    def test_delete_user_account(self):
+            '''
+            test_delete_user_account to test if we can remove an account from our account list
+            '''
+            self.new_account.save_user_account()
+            test_account = Credentials("Twit","Ian-arc","john.arch@email.com","1234567890k") # new user
+            test_account.save_user_account()
+
+            self.new_account.delete_user_account()# Deleting a user object
+            self.assertEqual(len(Credentials.accounts_list),1) 
+
+    def test_display_accounts(self):
+            '''
+            method that returns a list of all accounts saved
+            '''
+
+            self.assertEqual(Credentials.display_accounts(),Credentials.accounts_list)               
 
 if __name__ == '__main__':
     unittest.main()       
--- credentials.py
+++ credentials.py
@@ -2,37 +2,37 @@ class Credentials:
     """
     Class that generates new instances of credentials.
     """
-    credentials_list = []
+    accounts_list = []
 
 
     def __init__(self,account_name,user_name,email,password):
         """
         create arguments for self
         """
-        self.account_name = user_name
+        self.account_name = account_name
         self.user_name = user_name
         self.email = email
         self.password = password
        
 
-    def save_user_credentials(self):
+    def save_user_account(self):
         """ 
         save_user_credentials method saves user credentials objects into credentials_list
         """
-        Credentials.credentials_list.append(self)
+        Credentials.accounts_list.append(self)
 
-    def delete_user_credentials(self):
+    def delete_user_account(self):
 
         '''
-        delete_user_credentials method deletes a saved credentials from the credentials_list
+        delete_user_account method deletes a saved account from the accounts_list
         '''
 
-        Credentials.credentials_list.remove(self) 
+        Credentials.accounts_list.remove(self) 
         
     @classmethod
-    def display_all_accounts(cls):
+    def display_accounts(cls):
         '''
-        method that returns the credentials list
+        method that returns the accounts list
         '''
-        return cls.credentials_list   
+        return cls.accounts_list   
       
